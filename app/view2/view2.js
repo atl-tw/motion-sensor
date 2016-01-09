5d5a4e04c1793d33f5db1f632b77e4073b67a409
Kiana Tennyson <ktenny@thoughtworks.com>
2016-01-09 07:29:03 -0500

    Initial commit

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,37 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', ['$scope', function($scope) {
+  var client;
 
+  var mqttServer = 'askKianaforServerName.us';
+
+  function connectMqttClient() {
+    console.log("connecting mqtt client");
+    client = new Paho.MQTT.Client(mqttServer, 8080, 'clientId');
+    client.onConnectionLost = onConnectionLost;
+    client.onMessageArrived = onMessageArrived;
+
+    client.connect({onSuccess:onConnect});
+  };
+
+  function onConnect() {
+    console.log("onConnect");
+    client.subscribe("topic");
+    var message = new Paho.MQTT.Message("webapp onConnect");
+    message.destinationName = "topic";
+    client.send(message);
+  }
+
+  function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+      console.log("onConnectionLost:"+responseObject.errorMessage);
+    }
+  }
+
+  function onMessageArrived(message) {
+    console.log("onMessageArrived:"+message.payloadString);
+  }
+
+  connectMqttClient();
 }]);
