a29e7c80d4227d81d7384b75ba6767d06c9c3b79
Kiana Tennyson <ktenny@thoughtworks.com>
2016-01-13 14:56:03 -0500

    change icon when motion begins or ends

--- app/heatMap/heatMap.js
+++ app/heatMap/heatMap.js
@@ -11,12 +11,25 @@ angular.module('myApp.heatMap', ['ngRoute'])
 
 .controller('HeatMapCtrl', ['$scope', 'QueueReader', function($scope, QueueReader) {
 
+   $scope.sensors = {"esp8266-5c:cf:7f:7:46:7": {"name": "sensor1", "client": "esp8266-5c:cf:7f:7:46:7", "status": "offline-sensor"},
+                  "esp8266-5c:cf:7f:7:46:26": {"name": "sensor2", "client": "esp8266-5c:cf:7f:7:46:26", "status": "offline-sensor"}};
+
+   $scope.sensorStyle = function(sensorName) {
+      var sensor = $scope.sensors[sensorName];
+      return sensor.name + " sensor-attributes " + sensor.status;
+   }
+
   function callback(message) {
-    //    turn sensors red/green/blue here
     var json = parseMessage(message);
-    console.log(json["client"]);
-    console.log(json["new-motion"]);
-    console.log(json["end-motion"]);
+    var sensor = $scope.sensors[json.client];
+
+    if (angular.isDefined(json["new-motion"])) {
+      sensor.status = "active-sensor";
+    } else if (angular.isDefined(json["end-motion"])) {
+      sensor.status = "inactive-sensor";
+    }
+
+    $scope.$apply();
   }
 
   function parseMessage(message) {
